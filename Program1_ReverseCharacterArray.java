95ec932df7f319eda432e91ba35bc593c0c81f0e
MythiliPrabaharan <mythili.prabaharan13@gmail.com>
2021-08-22 18:42:32 +0530

    Add files via upload

---
+++ Program1_ReverseCharacterArray.java
@@ -0,0 +1,20 @@
+public class Program1_ReverseCharacterArray 
+{
+	public static void reverseString(char[] s)
+	{
+		char tmp = '0'; 
+		for(int i = 0; i < s.length / 2; i++)
+		{ 
+			tmp = s[i]; 
+			s[i] =s[s.length - 1 - i]; 
+			s[s.length - 1 - i] = tmp;
+		}
+		System.out.println(s);
+	}
+
+	public static void main(String[] args)
+	{
+		char[] s = {'h','e','l','l','o'};
+		reverseString(s);
+	}
+}
