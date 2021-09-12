a9d68407a68a00409084ef412cb1552f5f3d589b
MythiliPrabaharan <mythili.prabaharan13@gmail.com>
2021-09-12 21:43:22 +0530

    Update Program6_BinarySearchRecursion.java

--- Program6_BinarySearchRecursion.java
+++ Program6_BinarySearchRecursion.java
@@ -1,21 +1,8 @@
 import java.util.Arrays;
 
-public class BinarySearchRecursion 
+public class Program6_BinarySearchRecursion 
 {
 	//https://github.com/MythiliPrabaharan
-	public static int i = 0;
-	public static void main(String[] args)
-	{
-		int[] array = {5, 7, 55, 99, 34};
-		Arrays.sort(array);
-		//System.out.println(Arrays.toString(array));
-		int length = array.length;
-		int search = 55;
-		int left = 0;
-		int right = length - 1;
-		System.out.println(binarySearchRecursion(array,left,right,search)==-1 ? "Number not found" : "The position of the search element is "+binarySearchRecursion(array,left,right,search));
-
-	}
 	public static int binarySearchRecursion(int[] array, int left, int right, int search)
 	{
 		if(right>=left)
@@ -30,5 +17,15 @@ public class BinarySearchRecursion
 		}
 		return -1;
 	}
-
+	public static void main(String[] args)
+	{
+		int[] array = {5, 7, 55, 99, 34};
+		Arrays.sort(array);
+		//System.out.println(Arrays.toString(array));
+		int length = array.length;
+		int search = 55;
+		int left = 0;
+		int right = length - 1;
+		System.out.println(binarySearchRecursion(array,left,right,search)==-1 ? "Number not found" : "The position of the search element is "+binarySearchRecursion(array,left,right,search));
+	}
 }
