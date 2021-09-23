fd1a8bc012099616bd6dd04b6d426bd132c8713a
MythiliPrabaharan <mythili.prabaharan13@gmail.com>
2021-09-23 15:42:31 +0530

    Update Program6_BinarySearchRecursion.java

--- Program6_BinarySearchRecursion.java
+++ Program6_BinarySearchRecursion.java
@@ -26,6 +26,7 @@ public class Program6_BinarySearchRecursion
 		int search = 55;
 		int left = 0;
 		int right = length - 1;
+		
 		System.out.println(binarySearchRecursion(array,left,right,search)==-1 ? "Number not found" : "The position of the search element is "+binarySearchRecursion(array,left,right,search));
 	}
 }
